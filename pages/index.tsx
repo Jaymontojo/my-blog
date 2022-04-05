ed90989f0b2e5981287897e407262254718168df
Jay <julesmontojo1@gmail.com>
2022-04-06 03:51:20 +0900

    feat: set up basic home html

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-
+import { PostCard, Categories, PostWidget} from '../components'
 const posts = [
   {title: 'React Testing', excerpt: 'Learn React Testing'},
   {title: 'React with Tailwind', excerpt: 'Learn React with Tailwind'},
-
 ];
-const Home: NextPage = () => {
+export default function Home() {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -17,15 +15,13 @@ const Home: NextPage = () => {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className= 'lg:col-span-8 col-span-1'>
           {posts.map((post, index) => {
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
+            <PostCard post ={post}/>
           })};
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
-
+            <PostWidget />
+            <Categories />
           </div>
         </div>
       </div>
@@ -33,4 +29,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+
